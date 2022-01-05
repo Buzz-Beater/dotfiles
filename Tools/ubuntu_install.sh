1c13cabd92f4730e79e2cce0b78cdb415c369be4
Baoxiong Jia <17614277+Buzz-Beater@users.noreply.github.com>
2022-01-05 17:17:57 +0800

    Update ubuntu_install.sh
    removed useless installation

--- Tools/ubuntu_install.sh
+++ Tools/ubuntu_install.sh
@@ -50,13 +50,6 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install -f -y
 rm google-chrome-stable_current_amd64.deb
 
-# # teamviewer
-# cd ~/Downloads
-# wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-# sudo dpkg -i teamviewer_amd64.deb
-# sudo apt install -f -y
-# rm teamviewer_amd64.deb
-
 # # dropbox
 # cd ~/Downloads
 # wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
@@ -77,10 +70,10 @@ sudo gem install jekyll bundler
 bundle exec jekyll serve
 bundle install
 
-# # # java environment
-# # sudo apt install openjdk-11-jdk -y
+# # java environment
+# sudo apt install openjdk-11-jdk -y
 
-# # GitKraken
+# GitKraken
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb # gitkraken
 dpkg -i gitkraken-amd64.deb
 rm gitkraken-amd64.deb
@@ -99,51 +92,4 @@ rm gitkraken-amd64.deb
 # zsh
 sudo apt install zsh fonts-powerline -y
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" 
-
-# # CUDA
-# cd NVIDIA
-# sudo chmod a+x install_cuda.sh
-# sudo ./install_cuda.sh
-
-# # PyCharm installation
-# mkdir ~/Library
-# cd ~/Tools/JetBrains/
-# tar -xvf pycharm-professional-2018.2.2.tar.gz -C ~/Library
-# tar -xvf CLion-2018.2.2.tar.gz -C ~/Library
-# sudo chmod a+x ~/Library/pycharm-2018.2.2/bin/pycharm.sh
-# sudo chmod a+x ~/Library/clion-2018.2.2/bin/clion.sh
-
-# # applications
-# sudo add-apt-repository ppa:otto-kesselgulasch/gimp -y # gimp
-# sudo apt-add-repository ppa:octave/stable -y # octave
-# sudo add-apt-repository ppa:zarquon42/meshlab -y # meshlab
-# sudo apt update
-# sudo apt install gimp octave meshlab -y
-
-########################### Interactive installation parts #####################################
-
-# # MATLAB installation
-# cd ~/Tools/MATLAB2017a
-# sudo mkdir /media/cdrom
-# sudo mount R2017a_glnxa64_dvd1.iso /media/cdrom -o loop
-# sudo /media/cdrom/install
-# # copy license code: 09806-07443-53955-64350-21751-41297
-# sudo umount /media/cdrom
-# sudo mount R2017a_glnxa64_dvd2.iso /media/cdrom/ -o loop
-# sudo cp Crack/R2017a/bin/glnxa64/libmwservices.so /usr/local/MATLAB/R2017a/bin/glnxa64/
-# sudo umount /media/cdrom
-# sudo rm -rf /media/cdrom
-# # Possibly write permissions are restricted for matlab
-# sudo chown -R $USER: ~/.matlab
-# sudo /usr/local/MATLAB/R2017a/bin/matlab
-# sudo apt install matlab-support
-# # set path to matlab-support
-
-# cd ~/Library
-# ./pycharm-2018.2.2/bin/pycharm.sh
-# ./clion-2018.2.2/bin/clion.sh
-
-# # Removing folders from sidebar
-# # Edit the file in /etc/xdg/user-dirs.defaults
-# # the configuration in ~/.config/user-dirs.dirs should also be changed according to need
 
